5f3c657a6c37c0a09d4c585cfabd5972d1063deb
Mahidhar Venna <mahidhar.reddy@fxinnovation.com>
2018-11-28 09:44:59 -0500

    fix gem issues

---
+++ resources/aws_gem.rb
@@ -0,0 +1,55 @@
+chef_gem 'aws-sdk-cloudformation' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-cloudwatch' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-core' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-dynamodb' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-ec2' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-elasticloadbalancing' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-iam' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-kinesis' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-route53' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-s3' do
+    compile_time true
+    action :install
+end
+
+chef_gem 'aws-sdk-ssm' do
+    compile_time true
+    action :install
+end
+
